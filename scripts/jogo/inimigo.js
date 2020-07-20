27cf4585bc7dc4bc417c12e4c29cfbed63d1e508
K0rgana <anagrom1999@gmail.com>
2020-07-20 20:09:22 -0300

    adicionando movimento a inimigo

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,5 +1,13 @@
 class Inimigo extends Animacao{
 	constructor(matriz, imagem, x, larguraPer, alturaPer, larguraSprite, alturaSprite){
 		super(matriz, imagem, x, larguraPer, alturaPer, larguraSprite, alturaSprite)
+		this.velocidade = 10;
 	}
+	move(){
+    this.x = this.x -this.velocidade;
+
+    if (this.x < -this.larguraPer ) {
+      this.x = width;
+    }
+  }
 }
--- scripts/jogo/sketch.js
+++ scripts/jogo/sketch.js
@@ -8,35 +8,14 @@ let personagem;
 let inimigo;
 
 const matrizInimigo = [
-	[0, 0],
-  [104, 0],
-  [208, 0],
-  [312, 0],
-  [0, 104],
-  [104, 104],
-  [208, 104],
-  [312, 104],
-  [0, 208],
-  [104, 208],
-  [208, 208],
-  [312, 208],
-  [0, 312],
-  [104, 312],
-  [208, 312],
-  [312, 312],
-  [0, 418],
-  [104, 418],
-  [208, 418],
-  [312, 418],
-  [0, 522],
-  [104, 522],
-  [208, 522],
-  [312, 522],
-  [0, 626],
-  [104, 626],
-  [208, 626],
-  [312, 626],
-];
+	[0, 0],    [104, 0],    [208, 0],    [312, 0],
+	[0, 104],  [104, 104],  [208, 104],  [312, 104],
+  [0, 208],  [104, 208],  [208, 208],  [312, 208],
+  [0, 312],  [104, 312],  [208, 312],  [312, 312],
+  [0, 418],  [104, 418],  [208, 418],  [312, 418],
+  [0, 522],  [104, 522],  [208, 522],  [312, 522],  
+  [0, 626],  [104, 626],  [208, 626],  [312, 626],
+ ];
 
 function preload() {
 	imgCenario = loadImage('imagens/cenario/floresta.png');
@@ -49,7 +28,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imgCenario, 2);
   personagem = new Personagem(imgPersonagem);
-  inimigo = new Inimigo(matrizInimigo, imgInimigo, width -52, 52, 52, 104, 104);
+  inimigo = new Inimigo(matrizInimigo, imgInimigo, width -52, 82, 82, 104, 104);
   frameRate(20);
   somJogo.loop();
 }
