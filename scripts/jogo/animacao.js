b536709fa968a77acfc3d240b2ba4d655baab698
K0rgana <anagrom1999@gmail.com>
2020-07-20 15:14:29 -0300

    adicionando classe de animação

---
+++ scripts/jogo/animacao.js
@@ -0,0 +1,30 @@
+class Animacao{
+	constructor(matriz, imagem, x, larguraPer, alturaPer, larguraSprite, alturaSprite){
+		this.matriz = matriz;
+		this.imagem = imagem;
+		this.larguraPer = larguraPer;
+		this.alturaPer = alturaPer;
+		this.x = x;
+		this.y = height - this.alturaPer;
+		this.larguraSprite = larguraSprite;
+		this.alturaSprite = alturaSprite;
+
+		this.frameAtual = 0;
+	}
+	exibe(){
+    image(this.imagem, this.x, this.y,
+    			this.larguraPer, this.alturaPer,
+          this.matriz [this.frameAtual] [0],
+          this.matriz [this.frameAtual] [1], 
+          this.larguraSprite, this.alturaSprite);
+    
+    this.anima();
+  }
+  anima(){
+    this.frameAtual ++ ;
+    
+    if (this.frameAtual >= this.matriz.length - 1) {
+      this.frameAtual = 0;
+    }
+  }
+}
