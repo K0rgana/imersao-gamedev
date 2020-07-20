4c6a3ea7d29e63f86cfb133f3be9281ca44f08be
K0rgana <anagrom1999@gmail.com>
2020-07-20 12:01:56 -0300

    adicionando música de fundo

--- scripts/jogo/sketch.js
+++ scripts/jogo/sketch.js
@@ -1,15 +1,17 @@
 let imgCenario;
+let somJogo;
 
 let cenario;
 
 function preload() {
 	imgCenario = loadImage('imagens/cenario/floresta.png');
+	somJogo = loadSound('sons/airtone_resonance.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imgCenario, 2);
-
+  somJogo.loop();
 }
 
 function draw() {
