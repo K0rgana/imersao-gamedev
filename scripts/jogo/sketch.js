3ac3c4a9d009f979ceac3aba4604b0d4e138ef5b
K0rgana <anagrom1999@gmail.com>
2020-07-20 20:13:41 -0300

    refatorando classe personagem

--- scripts/jogo/sketch.js
+++ scripts/jogo/sketch.js
@@ -16,6 +16,12 @@ const matrizInimigo = [
   [0, 522],  [104, 522],  [208, 522],  [312, 522],  
   [0, 626],  [104, 626],  [208, 626],  [312, 626],
  ];
+const matrizPer = [
+  [0, 0],   [220, 0],   [440, 0],   [660, 0],
+  [0, 270], [220, 270], [440, 270], [660, 270],
+  [0, 540], [220, 540], [440, 540], [660, 540],
+  [0, 810], [220, 810], [440, 810], [660, 810],
+];
 
 function preload() {
 	imgCenario = loadImage('imagens/cenario/floresta.png');
@@ -27,7 +33,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imgCenario, 2);
-  personagem = new Personagem(imgPersonagem);
+  personagem = new Personagem(matrizPer,imgPersonagem, 30, 160, 185, 220, 270);
   inimigo = new Inimigo(matrizInimigo, imgInimigo, width -52, 82, 82, 104, 104);
   frameRate(20);
   somJogo.loop();
