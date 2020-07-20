bb8f3ee572fd8eb1c9c3021a538993d6cd64ee07
K0rgana <anagrom1999@gmail.com>
2020-07-20 12:55:12 -0300

    adicionando personagem

--- scripts/jogo/sketch.js
+++ scripts/jogo/sketch.js
@@ -1,20 +1,26 @@
 let imgCenario;
 let somJogo;
+let imgPersonagem;
 
 let cenario;
+let personagem;
 
 function preload() {
 	imgCenario = loadImage('imagens/cenario/floresta.png');
+	imgPersonagem = loadImage('imagens/personagem/correndo.png');
 	somJogo = loadSound('sons/airtone_resonance.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imgCenario, 2);
+  personagem = new Personagem(imgPersonagem);
+  frameRate(20);
   somJogo.loop();
 }
 
 function draw() {
 	cenario.exibe();
 	cenario.move();
+	personagem.exibe();
 }
