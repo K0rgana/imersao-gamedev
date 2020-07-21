e6862a5dfa6887a3df95c84c76b3b5048d314a60
K0rgana <anagrom1999@gmail.com>
2020-07-21 16:24:46 -0300

    adicionando parametros de velocidade e delay a inimigo

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,12 +1,15 @@
 class Inimigo extends Animacao{
-	constructor(matriz, imagem, x, variacaoY, larguraPer, alturaPer, larguraSprite, alturaSprite){
-		super(matriz, imagem, x, variacaoY, larguraPer, alturaPer, larguraSprite, alturaSprite)
-		this.velocidade = 10;
+	constructor(matriz, imagem, x, variacaoY, larguraPer, alturaPer, larguraSprite, alturaSprite, velocidade, delay){
+		super(matriz, imagem, x, variacaoY, larguraPer, alturaPer, larguraSprite, alturaSprite);
+
+		this.velocidade = velocidade;
+		this.delay = delay;
+		this.x = width + delay;
 	}
 	move(){
     this.x = this.x -this.velocidade;
 
-    if (this.x < -this.larguraPer ) {
+    if (this.x < -this.larguraPer - this.delay) {
       this.x = width;
     }
   }
