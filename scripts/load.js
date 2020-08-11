0c823e71e7ed9ad772be208ad1740e61e1e0dddb
K0rgana <anagrom1999@gmail.com>
2020-08-11 16:13:13 -0300

    refatorando sketch

---
+++ scripts/load.js
@@ -0,0 +1,13 @@
+function preload() {
+	imgCenario = loadImage('imagens/cenario/floresta.png');
+    imgGameOver = loadImage('imagens/assets/game-over.png');
+    
+    imgPersonagem = loadImage('imagens/personagem/correndo.png');
+    
+	imgInimigo = loadImage('imagens/inimigos/gotinha.png');
+	imgInimigoGrande = loadImage('imagens/inimigos/troll.png');
+    imgInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+    
+	somJogo = loadSound('sons/airtone_resonance.mp3');
+	somPulo = loadSound('sons/somPulo.mp3');
+}
