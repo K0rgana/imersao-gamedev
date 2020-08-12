85695c657aaeefbc26bfde67f002b97468210bca
K0rgana <anagrom1999@gmail.com>
2020-08-12 20:56:33 -0300

    corrigindo ordem de função

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,22 +1,23 @@
-function keyPressed(){
-	jogo.keyPressed(key);
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
   frameRate(20);
   somJogo.loop();
-
+  
   jogo = new Jogo();
   telaInicial = new TelaInicial();
   botaoGerenciador = new BotaoGerenciador('iniciar', width / 2, height /2);
   jogo.setup();
- 
+  
   cenas= {
     jogo,
     telaInicial
   };
+}
+
+function keyPressed(){
+  jogo.keyPressed(key);
 }
 
 function draw() {
